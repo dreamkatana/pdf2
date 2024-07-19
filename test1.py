3914944445c87371bf99ddae7a01c597d97e2e54
Joao Eduardo <joaoedu@gmail.com>
2024-07-19 12:35:53 -0300

    atualização testes

--- test1.py
+++ test1.py
@@ -5,7 +5,11 @@ pdf = FPDF(orientation="P", unit="mm", format="A4")
 
 pdf.add_page()
 pdf.set_font('helvetica', 'BIU', size=12)
+#CELL LARGURA DO BOX, ALTURA DO BOX
 pdf.cell(40, 10, text="hello world", border=1)
-pdf.cell(60, 10, 'Powered by FPDF.', new_x="LMARGIN", new_y="NEXT", align='C', border=1)
+pdf.cell(10)
+
+pdf.cell(40, 10, 'Powered by FPDF.', new_x="LMARGIN", new_y="NEXT", align='C', border=1)
+pdf.cell(40, 10, 'Teste by FPDF.', new_x="LMARGIN", new_y="NEXT", align='C', border=1)
 
 pdf.output("hello_world2.pdf")
